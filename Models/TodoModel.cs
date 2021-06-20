c84262f35c874626cebbab56e64862114b0d041d
ashsvis <ashsvis@gmail.com>
2021-06-20 15:56:37 +0300

    Add project files.

---
+++ Models/TodoModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TodoList.Models
+{
+    class TodoModel : INotifyPropertyChanged
+    {
+        private bool isDone;
+        private string text;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public bool IsDone
+        {
+            get { return isDone; }
+            set 
+            {
+                if (isDone == value)
+                    return;
+                isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set 
+            {
+                if (text == value)
+                    return;
+                text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Services/FileIOService.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.IO;
+using TodoList.Models;
+
+namespace TodoList.Services
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<TodoModel> LoadData()
+        {
+            bool fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<TodoModel>();
+            }
+            using (StreamReader reader = File.OpenText(PATH))
+            {
+                string input = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<TodoModel>>(input);
+            }
+        }
+
+        public void SaveData(object todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+    }
+}
